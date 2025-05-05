194827f20619dedf1e9db634982664741d596c08
Jang HyunBin <janghb3730@gmail.com>
2025-05-06 00:34:19 +0900

    Init : 프로젝트 초기설정

---
+++ springboot/src/main/java/com/develop25/trendit/TrenditApplication.java
@@ -0,0 +1,13 @@
+package com.develop25.trendit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrenditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrenditApplication.class, args);
+	}
+
+}
---
+++ springboot/src/test/java/com/develop25/trendit/TrenditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.develop25.trendit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrenditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
