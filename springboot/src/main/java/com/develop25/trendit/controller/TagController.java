f0874e9544c0647ecc171fd0a9fc32644e968bd0
119qwer <114053367+119qwer@users.noreply.github.com>
2025-05-19 23:41:40 +0900

    상품등록 브랜치 코드 수정 반영

--- springboot/src/main/java/com/develop25/trendit/controller/TagController.java
+++ springboot/src/main/java/com/develop25/trendit/controller/TagController.java
@@ -3,14 +3,20 @@ package com.develop25.trendit.controller;
 import com.develop25.trendit.domain.Image;
 import com.develop25.trendit.domain.Product;
 import com.develop25.trendit.domain.Tag;
+import com.develop25.trendit.dto.ImageUploadRequest;
 import com.develop25.trendit.repository.ProductImageRepository;
 import com.develop25.trendit.repository.ProductRepository;
 import com.develop25.trendit.repository.TagRepository;
 import com.develop25.trendit.service.ImageTagService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.List;
@@ -34,6 +40,26 @@ public class TagController {
     @Value("${openai.api.key}")
     private String openaiApiKey;
 
+    private  ImageUploadRequest imageUploadRequest;
+
+    @Operation(
+            summary = "태그 생성",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    content = @Content(
+                            mediaType = "multipart/form-data",
+                            schema = @Schema(implementation = ImageUploadRequest.class)
+                    )
+            )
+    )
+    @PostMapping(value = "/generate-tags", consumes = "multipart/form-data")
+    public ResponseEntity<List<String>> generateTags(@ModelAttribute ImageUploadRequest request) throws IOException {
+
+        MultipartFile file = request.getFile();
+        byte[] imageBytes = file.getBytes();
+        List<String> tags = tagService.generateTags(imageBytes);
+
+        return ResponseEntity.ok(tags);
+    }
 
 
     @PostMapping("generate-tags/{productId}")
--- springboot/src/main/java/com/develop25/trendit/dto/ImageUploadRequest.java
+++ springboot/src/main/java/com/develop25/trendit/dto/ImageUploadRequest.java
@@ -10,6 +10,6 @@ import org.springframework.web.multipart.MultipartFile;
 @AllArgsConstructor
 @ToString
 public class ImageUploadRequest {
-    @Schema(description = "업로드할 이미지 파일", type = "string", format = "binary")
+    @Schema(description = "업로드할 이미지 파일", type = "string", format = "binary", required = true)
     private MultipartFile file;
 }
