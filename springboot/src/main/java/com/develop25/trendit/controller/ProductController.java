c7a798368b419d8d591d1cdded0996b9a8ee026e
119qwer <114053367+119qwer@users.noreply.github.com>
2025-05-20 22:51:43 +0900

    불필요 api 제거

--- springboot/src/main/java/com/develop25/trendit/controller/ProductController.java
+++ springboot/src/main/java/com/develop25/trendit/controller/ProductController.java
@@ -44,69 +44,6 @@ public class ProductController {
     @Autowired
     private UserRepository userRepository;
 
-    //POST
-    //상품 등록 api
-
-    @PostMapping("/api/productCreate")
-    public Product create(@org.springframework.web.bind.annotation.RequestBody ProductDTO dto){
-        //이 userId는 get으로 받아야 하는 데 일단 임시로 1로 해준것임($$)
-        //확신은 못하지만 로그인 정보를 통해서 user테이블에 접근해서 user_id를 가져올 거임
-        Long userId = 1L;
-        Product product = dto.toEntity();
-
-        return productRepository.save(product);
-    }
-
-    //PATCH
-    //상품 수정 api
-    @PatchMapping("/api/product/{productId}")
-    public ResponseEntity<Product> update(@PathVariable Long productId, @org.springframework.web.bind.annotation.RequestBody ProductDTO dto){
-        Long userId = 1L;
-        //dto 변환
-        Product product = dto.toEntity();
-        //타겟 조회
-        Product target = productRepository.findById(productId).orElse(null);
-        //잘못된 요청 처리
-        //해당 id가 없거나 수정요청한 id와 db의 id가 다른 경우 상태메세지 400 리턴
-        if(target == null || productId != target.getProductId()){
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
-        }
-        target.patch(product);
-        Product updated = productRepository.save(target);
-
-        return ResponseEntity.status(HttpStatus.OK).body(updated);
-
-    }
-
-    //DELETE
-    //상품 삭제 api
-    @DeleteMapping("/api/product/{productId}")
-    public ResponseEntity<Product> delete(@PathVariable Long productId){
-        //타겟 조회
-        Product target = productRepository.findById(productId).orElse(null);
-        //잘못된 요청 처리
-        if(target == null){
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-        }
-        //대상 삭제
-        productRepository.delete(target);
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
-
-
-    }
-
-    //POST
-    //이미지 등록 api
-    @Operation(summary = "상품 이미지 등록 API")
-    @PostMapping("/api/productCreate/image")
-    public Image createProduct(
-            @Parameter(description = "업로드할 이미지 파일", required = true)
-            @RequestParam("file") MultipartFile file
-    ) throws IOException {
-        Image image = new Image();
-        image.setImage(file.getBytes());
-        return productImageRepository.save(image);
-    }
 
     //swagger의 multipart/form-data 인식을 위한 어노테이션
     @Operation(
