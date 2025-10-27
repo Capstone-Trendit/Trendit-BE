7e7f2e2fec16eb0d4316fe4b80aac65767fe7a3e
119qwer <114053367+119qwer@users.noreply.github.com>
2025-10-27 14:06:04 +0900

    태그 api 수정

--- springboot/src/main/java/com/develop25/trendit/controller/TagController.java
+++ springboot/src/main/java/com/develop25/trendit/controller/TagController.java
@@ -98,13 +98,12 @@ public class TagController {
     public List<Tag> getTagsByProductId(@PathVariable Long productId) {
         return tagRepository.findByProducts_ProductId(productId);
     }
+
     @Operation(description = """
         입력한 태그 목록(tags)을 상품명(name)으로부터 추출한 네이버 쇼핑 대표 카테고리 태그와 비교하여,
         유사도가 임계값(기본 0.7) 이상이면 해당 태그로 치환한 최종 태그 리스트를 반환
         """)
-    @PostMapping(path = "/tagChange",
-            consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(path = "/tagChange")
     public List<String> getTagChange(@ModelAttribute TagChangeRequest req) {
         String name = req.getName();
         List<String> tags = req.getTags();
